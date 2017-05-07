8302c0c1b3fc3784cbbcf0f2e77e400f8c47c158
Constantine Vesna <Constantine Vesna>
2017-05-07 01:18:00 -0400

    initial commit

---
+++ game.js
@@ -0,0 +1,34 @@
+"use strict";
+
+class Game {
+  constructor (player1, player2, deck1, deck2) {
+
+  }
+  start () {}
+  nextTurn () {}
+  finish () {}
+}
+
+class Deck {
+  draw (n = 1) {}
+}
+
+class Hand {
+  play (card_id) {} // ?
+  add(card)
+}
+
+
+class Mana {
+  constructor (player) {
+
+  }
+  increment (n) {}
+  get amount () {}
+}
+
+class Player {
+  constructor () {}
+  get hp () {}
+
+}

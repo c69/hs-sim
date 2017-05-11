1541944b32790be220d1f68d50734eb5b20f44c1
Constantine Vesna <Constantine Vesna>
2017-05-11 18:23:42 -0400

    moved cards to separate module. Milestone 2 complete.

--- main.js
+++ main.js
@@ -6,64 +6,9 @@ const Deck = require('./classes/deck.js');
 const Player = require('./classes/player.js');
 
 const Minion = require('./classes/minion.js');
-
-/**
- * intentionally non-generic card class / API sketch
- */
-class FireballCard {
-  constructor () {
-    this.type = "spell";
-    this.price = 4;
-    this.name = 'Fireball';
-  }
-  action (fireballTarget) {
-    fireballTarget.damage(3);
-    //return chooseTarget(['minion', 'hero']).damage(6);
-    //return chooseTarget(['minion', 'hero'], spell.dealDamage(6));
-    //return dealDamageToSingleTarget(6, ['minion', 'hero']);
-  }
-}
-
-class JunkCard {
-  constructor (name, price) {
-    this.type = "spell";
-    this.price = price;
-    this.name = name;
-
-    this.action = this.action.bind(this); // ES6 caveman bind
-  }
-  action () {
-    console.log(`nothing happens [${this.price} mana wasted..]`)
-  }
-}
-
-class MinionCard {
-  constructor (minion, price) {
-    this.type = "minion";
-    this.price = price;
-    this.name = minion.name;
-    this.health = minion.health;
-    this.attackPower = minion.attackPower;
-    this.minion = minion;
-    this.owner = null; // is it ??? should the owner of card be the owner of hand ?!
-
-    this.action = this.action.bind(this); // ES6 caveman bind
-  }
-  action (player) {
-    //player.board.choosePosition(); ??
-    this.minion.owner = player;
-    player.board.addOwn(player, this.minion);
-    //player.summonMinion(this.minion);
-  }
-  isPlayable (player) { // this could become a _God_ function :/, consider using target: []
-    if (player.board.listOwn(p1).minions.length > 6) return false;
-    if (player.mana < this.price) return false;
-    return true;
-  }
-  toString () {
-    return [`Object Card: ${this.name}`];
-  }
-}
+const MinionCard = require('./classes/cards.js').MinionCard;
+const FireballCard = require('./classes/cards.js').FireballCard;
+const JunkCard = require('./classes/cards.js').JunkCard;
 
 var fireballs = [];
 for (let i = 0; i < 30; i++) {

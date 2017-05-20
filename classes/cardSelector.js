0cbe8b5e59b11e6b4022eefc366f8c319914ce14
Constantine Vesna <Constantine Vesna>
2017-05-20 04:30:13 -0400

    cardSelector - implemented type and prop search. Added jsdoc, some refactoring and cleanup.

--- classes/cardSelector.js
+++ classes/cardSelector.js
@@ -1,5 +1,5 @@
 const Player = require('./player.js');
-const CardJSON = require('../data/cards.generated.json');
+const CardJSON = require('../data/cards.all.generated.json');
 
 
 const ZONES = {
@@ -57,6 +57,22 @@ class Card {
       this.rarity = cardDef.rarity;
 
       this.cost = cardDef.cost;
+      this.overload = cardDef.overload;
+
+      this.tags = cardDef.tags || [];
+
+      this.play = cardDef.play;
+      this.target = cardDef.target;
+      //this.chooseOne = ???
+      //this.joust = ???
+      
+      this.death = cardDef.death; 
+      // this.tags.push(cardDef.death)
+      this.trigger = cardDef.trigger;
+      this.aura = cardDef.aura;
+      // this.secret = cardDef.secret;
+      // this.quest = cardDef.quest;
+
 
       this.zone = ZONES.deck;
       this.owner = owner;
@@ -64,26 +80,41 @@ class Card {
       this.deck_id = deck_id++;  
     }
     _draw () {
-        this.zone = ZONES.hand
+        if (this.zone !== ZONES.deck) throw 'Attempt to draw NOT from deck';
+        this.zone = ZONES.hand;
     }
     _play () {
         if (this.zone !== ZONES.hand) throw 'Attempt to play card NOT from hand';
-
         this.zone = ZONES.aside;
-        this.play();
+
+        // if (this.target) {
+        //   asyncChooseTarget, OR expect it to be provided in arguments ?
+        //   btw, asynctChoosePosition ? - should work the same way ..
+        // };
+
+        // todo: consider splitting this IF so proper event could be emitted
         if (this.type === TYPES.minion || this.type === TYPES.weapon) {
-            this.zone = ZONES.play
+            this.zone = ZONES.play;
         } else if (this.type === TYPES.spell) {
             this.zone = this.isSecret ? ZONES.secret : ZONES.grave;
         } else {
             throw `Played card of unplayable type:${this.type}`;
         }
+        
+        this.play({self, $, game, target, position}); // battlecry !
+
     }
     _mill () {
-        this.zone = ZONES.grave
+        this.zone = ZONES.grave;
     }
     _die () {
-        this.zone = ZONES.grave
+        this.death && this.death({self, $, game}); // deathrattle
+        this.zone = ZONES.grave;
+    }
+    _copy () {
+        let copy = new this.prototype.constructor(this, this.owner);
+        // copy.tags[] are DIRTY !
+        copy.zone = ZONES.aside; 
     }
 }
 
@@ -168,6 +199,7 @@ let card_defs = CardJSON.filter(v => v.collectible === true);
     for (let i = 0; i < 30; i++) {
         let dice = Math.floor(Math.random()*(card_defs.length - 1));
         let card = card_defs[dice];
+        
         let structor = {
             [TYPES.minion]: Minion,
             [TYPES.hero]: Hero,
@@ -180,47 +212,65 @@ let card_defs = CardJSON.filter(v => v.collectible === true);
     }
 });
 
-class GameBoardWithZonesAndCardSelector {
+class Board2 {
   constructor (deck1, deck2, player1, player2) {
     this.deck1 = deck1;
     this.deck2 = deck2;
     this.player1 = player1;
     this.player2 = player2;
   }
-  $ (selector_string, player) {
+  /** 
+   * Declarative Array.filter on steroids. Uses DSL for queries:
+   * - Owner: [any|own|enemy] XOR //TODO: should it be like this ? (default = any)
+   * - Type: [card|character|minion|hero|weapon|power|spell] OR (default = card)
+   *   where 'card' is any card, 'character' is 'hero OR minon'
+   *   (!) enchantments are not searchable, at least for now
+   * - Zone: @[deck|hand|play|grave|aside|secret] OR (default = @play)
+   * - Tags: #[tagName] check if card has tag currently //TODO: add full list
+   *   e.g: 'minion #taunt' or 'minion #battlecry'
+   * - Prop: .[propertyName][<|>|=|!=|<=|>=], read any property from card object
+   *   and check boolean, string or number value
+   *   e.g.: 'minion .attack<3' or 'minion race=murloc' or 'character .isReady'
+   * - (NOT IMPLEMENTED) Negation: [!self|!.prop] for the real world cases when you need to exclude something
+ 
+   * @param {Player} player Could (and should) be curried for card helper function (player is self.owner)
+   * @param {string} selector Refer to syntax above 
+   */
+  $ (player, selector_string) {
     let [ownPlayer, enemyPlayer] = this.player1 === player ? [this.player1, this.player2] : [this.player2, this.player1];
 
     let tokens = selector_string.split(/\s+/);
     let filters = [];
 
-    //card owner: XOR 
+    //card owner: choose one - XOR 
     if (!tokens.includes('any')) {
       if (tokens.includes('enemy')) {
-        filters.push(v = v.owner !== own);  
+        filters.push(v = v.owner !== ownPlayer);  
       } else if (tokens.includes('own')) {
-        filters.push(v = v.owner === own);
+        filters.push(v = v.owner === ownPlayer);
       }
     }
 
-    //card type: AND
+    //card type: BROADEN the search - OR
+    let allowed_types = [];  
     if (!tokens.includes('card')) {
-      let allowed_types = [];  
+
       if (tokens.includes('minion')) {
-        allowed_types.push('minion');  
+        allowed_types.push(TYPES.minion);  
       }
       if (tokens.includes('hero')) {
-        allowed_types.push('hero');  
+        allowed_types.push(TYPES.hero);  
       } 
       if (tokens.includes('character')) {
-        allowed_types.push('hero', 'minion');  
+        allowed_types.push(TYPES.hero, TYPES.minion);  
       } 
       if (tokens.includes('weapon')) {
-        allowed_types.push('weapon');  
+        allowed_types.push(TYPES.weapon);  
       }
       if (tokens.includes('spell')) {
-        allowed_types.push('spell');  
+        allowed_types.push(TYPES.spell);  
       }
-      console.log(allowed_types);
+      console.log('types', allowed_types);
       filters.push(v => allowed_types.includes(v.type));
     }
 
@@ -231,21 +281,45 @@ class GameBoardWithZonesAndCardSelector {
     var zoneSelectors = tokens.filter(v => /^@[a-z]+/i.test(v));
     console.log('zoneSelectors', zoneSelectors);
 
+    //property selectors only NARROW the search, so its AND
     let propRegexp = /^\.[0-9a-z]+((=|!=|<|>|<=|>=)[0-9a-z]+){0,1}$/i;
     //.test('.prop<42')
     var propSelectors = tokens.filter(v => propRegexp.test(v));
     console.log('propSelectors', propSelectors);
+    let propFilters = propSelectors.map(selector => {
+        let [
+          _match, // destructuring will throw, if regex match fails
+          propertyName,
+          operator,
+          comparisonValue
+        ] = selector.match(/^\.([0-9a-z]+)(!=|<=|>=|<|>|=)?(.*)$/);
+       
+       if (!operator) {
+         return v => !!v.propertyName;
+       }
+       if (!comparisonValue) throw new SyntaxError('Selector must have comparison value, when comparison operator is provided');
+       // add type/NaN checking - as only Numbers should allow < and >       
+       let ops = {
+         '<': (v) => v[propertyName] < comparisonValue,
+         '>': (v) => v[propertyName] > comparisonValue,
+         '>=': (v) => v[propertyName] >= comparisonValue,
+         '<=': (v) => v[propertyName] <= comparisonValue,
+         '=': (v) => v[propertyName] === comparisonValue || new RegExp('^' + comparisonValue + '$', 'i').test(v[propertyName]), 
+         '!=': (v) => v[propertyName] != comparisonValue,
+       };       
+       return ops[operator];
+       //return new Function('v', `v.${propertyName} ${operator} ${comparisonValue}`);
+    });
     //return tokens;  
     
     let all_cards = [...this.deck1, ...this.deck2];
-    //console.log(filters[0].toString());
-    return filters.reduce((a,v) => a.filter(v), all_cards);
-    //return all_cards.map(v=>v.type);
-    //return [...this.deck1, ...this.deck2].filter(v => v.zone === ZONES.play);
+    console.log(allowed_types, filters[0].toString());
+    //console.log(all_cards.map(v=>v.type));
+    return [...filters, ...propFilters].reduce((a,v) => a.filter(v), all_cards);
   }
 }
 
-let s = new GameBoardWithZonesAndCardSelector(arr1, arr2, p1, p2);
+let s = new Board2(arr1, arr2, p1, p2);
 
 //console.log(s.$('any card @deck .cost #taunt #divineShield', p2).map(v=>v.name));
-console.log(s.$('any card @deck', p2).map(v=>v.name));
+console.log(s.$(p2, 'any @deck minion .race=beast').map(v=>v.cost + ' ' + v.name));

d626ea6e6b48919a0838c82f98c197705416347e
Constantine Vesna <Constantine Vesna>
2017-05-24 04:14:39 -0400

    added game steps enumeration in comments

--- classes/game.js
+++ classes/game.js
@@ -4,6 +4,31 @@
 const Board = require('./board.js');
 
 /**
+ * 
+ 
+http://hearthstone.gamepedia.com/GameTag_enumeration
+
+http://hearthstone.gamepedia.com/Step_enumeration
+INVALID 0 
+BEGIN_FIRST 1 
+BEGIN_SHUFFLE 2 
+BEGIN_DRAW 3 
+BEGIN_MULLIGAN 4 
+MAIN_BEGIN 5 MAIN_READY 6 Player tags are reset/incremented (RESOURCES, COMBO_ACTIVE, NUM_CARDS_DRAWN_THIS_TURN, etc) 
+MAIN_RESOURCE 7 
+MAIN_DRAW 8 
+MAIN_START 9 
+MAIN_ACTION 10 
+MAIN_COMBAT 11 
+MAIN_END 12 End of Turn triggers. 
+MAIN_NEXT 13 
+FINAL_WRAPUP 14 
+FINAL_GAMEOVER 15 
+MAIN_CLEANUP 16 
+MAIN_START_TRIGGERS 17 
+
+
+
 // g.start();
 // g.end();
 

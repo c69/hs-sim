ca6f51ec42e9d4e2c160ff9c1215ad13bde388c9
Constantine Vesna <Constantine Vesna>
2017-05-27 19:17:54 -0400

    refactoring: cleanup game.js, extract combat.js, make viewAvailableOptions functional and stateless

---
+++ classes/combat.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const {
+  TAGS,
+  //EVENTS
+} = require('../data/constants.js');
+
+ /** 
+   * Execute combat action
+   * @param {MinionCard|HeroCard} attacker
+   * @param {MinionCard|HeroCard} target
+   * @param {?Game} game
+   */
+  function combat (attacker, target, game) {
+    //console.log(`Attacking ${attacker} -> ${target}`);  
+    if (!target) throw 'no target'; //return;
+    if (target.health < 1) throw 'dead target'; //return;
+    if (attacker.health < 1) throw 'dead attacker'; //return;
+    if (attacker.owner !== game.activePlayer) throw 'wrong turn'; //return; // is there a way to attack on enemy turn ? - UNGORO:WarriorLegendDino(8)
+    if (target.owner === attacker.owner) throw 'own unit'; //return; // will fail for Hunter:Misdirection secret, and Ogres
+    if (attacker.tags.includes(TAGS.windfury)) {
+      if (attacker.attackedThisTurn > 1) throw 'already attacked too many times this turn'; //return
+    } else {
+      if (attacker.attackedThisTurn > 0) throw 'already attacked this turn'; //return
+    }
+    console.log(`⚔️ ${attacker.name}(${attacker.attack}/${attacker.health}) attacks ${target.name}(${target.attack}/${target.health})`);
+    //console.log(`🛡️ ${attacker.name} attacks ${target.name}(${target.attack}/${target.health})`);
+    
+    // this looks like generic Card._damageApply(n)
+    [
+      [attacker, target.attack],
+      [target, attacker.attack]
+    ].forEach(([character, dmg]) => character._damageApply(dmg));
+    
+    attacker.attackedThisTurn += 1;  
+        
+    //console.log(`⚔️ end----`);
+  }
+
+  module.exports = combat;
--- classes/game.js
+++ classes/game.js
@@ -1,6 +1,8 @@
 'use strict';
 // @ts-check
 
+const combat = require('./combat.js');
+
 const Board = require('./board.js');
 const {
   TAGS,
@@ -18,18 +20,22 @@ BEGIN_FIRST 1
 BEGIN_SHUFFLE 2 
 BEGIN_DRAW 3 
 BEGIN_MULLIGAN 4 
+
 MAIN_BEGIN 5 MAIN_READY 6 Player tags are reset/incremented (RESOURCES, COMBO_ACTIVE, NUM_CARDS_DRAWN_THIS_TURN, etc) 
 MAIN_RESOURCE 7 
 MAIN_DRAW 8 
 MAIN_START 9 
+? MAIN_START_TRIGGERS 17 
+
 MAIN_ACTION 10 
 MAIN_COMBAT 11 
+? MAIN_CLEANUP 16 
+
 MAIN_END 12 End of Turn triggers. 
 MAIN_NEXT 13 
+
 FINAL_WRAPUP 14 
 FINAL_GAMEOVER 15 
-MAIN_CLEANUP 16 
-MAIN_START_TRIGGERS 17 
 
 
 
@@ -63,9 +69,6 @@ class Game {
     this.activePlayer = this.players[this.turn % 2]; //this is a copypaste
     this.passivePlayer = this.players[(this.turn + 1) % 2]; //this is a copypaste
     
-    this.options = {
-      actions: []
-    };
     //this.observableState = {}; // ?
     //this.fullState = {}; // ??? 
   }
@@ -75,8 +78,6 @@ class Game {
       player.draw(5);
       player.manaCrystals = 1;
       player.mana = player.manaCrystals;
-
-      //player.board = this.board;// temporary hack - wiring
     });
 
   }
@@ -119,20 +120,17 @@ class Game {
     this.isOver = false;
     this._init();//maybe with rules ? like min/max mana, etc
 
-    this._refreshAvailableOptions(); // cancer 
     return this;  
   }
   end () {
     this.isOver = true;
 
-    this._refreshAvailableOptions(); // cancer
     return this;  
   }
   concede () {
     this.activePlayer.loose();  
     this.isOver = true;
     
-    this._refreshAvailableOptions(); // cancer
     return this;  
   }
   disconnect () {
@@ -143,31 +141,24 @@ class Game {
     this._onTurnStart();
     this._attemptToDrawCard();
     
-    this._refreshAvailableOptions(); // cancer 
     return this;        
   }
-  usePower (target_idx) {
-    //todo: implement me
-    this._refreshAvailableOptions(); // cancer  
-    return this;  
-  }
-  attack (attacker_idx, target_idx) {//(1,1) Elf2 -> Zomb1
-    let o = this.options.actions[attacker_idx];
+  //-- intra turn action --------------
+  attack (attacker_idx, target_idx) {
+    let o = this.viewAvailableOptions().actions[attacker_idx];
+
     let a = o.unit;
     let t = o.targetList[target_idx];
     // if Ogre retarget - choose new target :)
     //this._onBeforeAttack(a, t);
-    this.combat(a, t);
+    combat(a, t, this);
 
-    //this._deathSweep(); // can only happen after action or tirggers
-    this._refreshAvailableOptions(); // cancer
     return this;
   }  
   playCard (card_idx = 0, position_idx = 0, target_idx = 0) {
     //console.log(`${this.activePlayer.name} tries to play ${card_idx}`);
-    let card = this.options.actions[card_idx];
+    let card = this.viewAvailableOptions().actions[card_idx];
     
-    //console.log('play card', c);
     if (card.targetList) { // spell-fireball OR battlecry
       var target = card.targetList[target_idx];  // if c.t      
     }
@@ -192,34 +183,14 @@ class Game {
 
     //console.log(`${this.activePlayer.name} chosen OPTION ${card_idx}`);
 
-    this._refreshAvailableOptions(); // cancer
     return this;
   }
-  _example_viewAvailableOptions () {
-    return {
-      actions: [
-          {id: 'minion1', type: 'ATTACK', name: 'Elf1', targetList: []},
-          {id: 'card1', type: 'CARD', name: 'Phyreball', targetList: []},
-          {id: 'card2', type: 'CARD', name: 'Elf', positionList: [0,1], targetsList: []}
-      ]  
-    }    
-  }
-  viewAvailableOptions () { // why not just access prop directly ? 
-    return this.options;  
-  } 
-  chooseOption (options_idx = 0, position_idx = 0, target_idx = 0) {
-    if (!this.options.actions.length) throw 'options.actions are empty' //return;
-    let o = this.options.actions[options_idx];
-    if (!o) throw new RangeError('Invalid option index provided.');
-    //console.log(o.type);
-    if (o.type === 'A') this.attack(options_idx, target_idx);
-    if (o.type === 'C') this.playCard(options_idx, position_idx, target_idx);
+  usePower (target_idx) {
+    //todo: implement me
 
-    this._nextTick();
-    this._refreshAvailableOptions(); // cancer
-    return this;
-  }
-  _nextTick () {
+    return this;  
+  }  
+  _cleanup () {
     //death logic onwards
     let deathrattle_list = [];
     this.board.$(this.activePlayer, 'character').forEach(character => {
@@ -243,21 +214,47 @@ class Game {
       }
     });
   }
+  /** @todo convert to documentation / TS definition */
+  _example_viewAvailableOptions () {
+    return {
+      actions: [
+          {id: 'minion1', type: 'ATTACK', name: 'Elf1', targetList: []},
+          {id: 'card1', type: 'CARD', name: 'Phyreball', targetList: []},
+          {id: 'card2', type: 'CARD', name: 'Elf', positionList: [0,1], targetsList: []}
+      ]  
+    }    
+  }
+  chooseOption (options_idx = 0, position_idx = 0, target_idx = 0) {
+    let opts = this.viewAvailableOptions().actions;
+    if (!opts.length) throw 'options.actions are empty' //return;
+    
+    let o = opts[options_idx];
+    if (!o) throw new RangeError('Invalid option index provided.');
+    //console.log(o.type);
+    if (o.type === 'A') this.attack(options_idx, target_idx);
+    if (o.type === 'C') this.playCard(options_idx, position_idx, target_idx);
+    //if (o.type === 'UP') this.usePower(options_idx, target_idx);
+
+    this._cleanup();
+
+    return this;
+  }
   /**
    *  A nice GOD method
    */
-  _refreshAvailableOptions () { try {
+
+
+  viewAvailableOptions () { try {
     //console.log(`refreshing options for ${this.activePlayer.name} on turn#${this.turn}`);
     if (!this.isStarted || this.isOver) {
       console.log('No options are available - game state is wrong.');
-      this.options = {
+      return {
         actions: []
       };
-      return;
-    } 
-    // board STILL(!) does not provide a way to simply list all (own)units, yet.
-    // PROBLEM - hero is not in BOARD yet. because he is legacy class, insted of card 
-    let pawns = this.board.$(this.activePlayer, 'own character');
+    }
+    let $ = this.board.$.bind(this.board, this.activePlayer);
+ 
+    let pawns = $('own character');
     let warriors = pawns.filter(v => {
       if (v.attack < 1) return false;
       if (!v.isReady && !v.tags.includes(TAGS.charge)) return false;
@@ -270,17 +267,12 @@ class Game {
       return v.attackedThisTurn < MAX_ATTACKS_ALLOWED_PER_TURN;
     });
     
-    let aubergines = this.board.$(this.activePlayer, 'enemy character');
-    let sheeps = aubergines
-      .concat(this.passivePlayer.hero) // temporary hack, until hero is refactored to be a Card
-      .filter(v => {
+    let aubergines = $('enemy character');
+    let sheeps = aubergines.filter(v => {
       return v.health > 0;
     });
 
     //scan for taunt
-    // todo: separate bags for meelee, spell, etc targets - GOD OBJECT CHECKER ORACLE !!!1111
-    //let hasTaunt = sheeps.some(v => v.buffs.includes('TAUNT'));
-    //if (hasTaunt) sheeps = sheeps.filter(v => v.buffs.includes('TAUNT'));
     let hasTaunt = sheeps.some(v => v.tags && v.tags.includes('TAUNT'));
     if (hasTaunt) sheeps = sheeps.filter(v => v.tags.includes('TAUNT'));
     
@@ -299,10 +291,8 @@ class Game {
         targetList: sheeps  
       };
     }).filter(v => v.targetList.length > 0);
-    //let mana = this.mana; // hand.listPlayable already checks for mana cost
-    let $ = this.board.$.bind(this.board, this.activePlayer);
 
-    let canSummonMore = (pawns.length < 8); // with hero
+    let canSummonMore = (pawns.length <= 7); // with hero
     
     let cards = this.activePlayer.hand.listPlayable().filter((v) =>{
       if (v.type === 'MINION') {
@@ -320,14 +310,14 @@ class Game {
         name: v.name,
         cost: v.cost,
         positionList: [0], //this.board.listOwn(this.activePlayer).minions.map((v,i)=>i), //slots between tokens, lol ? //?    
-        targetList: v.target && $(v.target), 
-        //targetList: sheeps.reverse() // for battlecry - starts from minions, to help Bob kill taunter Bears )
+        targetList: v.target && $(v.target)
       };
     });
 
     // i'd like options to just be a flat array (of actions), but sometimes i STILL need a debug info
     //console.log('actions --', attack, cards);
-    this.options = {
+    //this.options = {
+    return {    
       actions: [
         ...attack,
         ...cards
@@ -335,36 +325,8 @@ class Game {
       //, aubergines
     };
   } catch(err) {console.warn.err}}
-  /** 
-   * Execute combat action
-   * @param {Character} attacker
-   * @param {Character} target
-   */
-  combat (attacker, target) {
-    //console.log(`Attacking ${attacker} -> ${target}`);  
-    if (!target) throw 'no target'; //return;
-    if (target.health < 1) throw 'dead target'; //return;
-    if (attacker.health < 1) throw 'dead attacker'; //return;
-    if (attacker.owner !== this.activePlayer) throw 'wrong turn'; //return; // is there a way to attack on enemy turn ? - UNGORO:WarriorLegendDino(8)
-    if (target.owner === attacker.owner) throw 'own unit'; //return; // will fail for Hunter:Misdirection secret, and Ogres
-    if (attacker.tags.includes(TAGS.windfury)) {
-      if (attacker.attackedThisTurn > 1) throw 'already attacked too many times this turn'; //return
-    } else {
-      if (attacker.attackedThisTurn > 0) throw 'already attacked this turn'; //return
-    }
-    console.log(`⚔️ ${attacker.name}(${attacker.attack}/${attacker.health}) attacks ${target.name}(${target.attack}/${target.health})`);
-    //console.log(`🛡️ ${attacker.name} attacks ${target.name}(${target.attack}/${target.health})`);
-    
-    // this looks like generic Card._damageApply(n)
-    [
-      [attacker, target.attack],
-      [target, attacker.attack]
-    ].forEach(([character, dmg]) => character._damageApply(dmg));
-    
-    attacker.attackedThisTurn += 1;  
-        
-    //console.log(`⚔️ end----`);
-  }
+
+  //-----------------------------------
   view () {
     console.log(`turn # ${this.turn}: ${this.activePlayer.name}`);
     this.players.forEach(player => {

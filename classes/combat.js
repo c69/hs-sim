5deb353e08420a43e5ba3b06293e19a573c75d59
Constantine Vesna <Constantine Vesna>
2017-05-30 12:45:16 -0400

    richer warnings for dead attacker (for bug: chain death of unstable ghouls does not cause deathsweep)

--- classes/combat.js
+++ classes/combat.js
@@ -14,8 +14,9 @@ const {
   function combat (attacker, target, game) {
     //console.log(`Attacking ${attacker} -> ${target}`);  
     if (!target) throw 'no target'; //return;
-    if (target.health < 1) throw 'dead target'; //return;
-    if (attacker.health < 1) throw 'dead attacker'; //return;
+    if (target.health < 1) throw `dead target ${target.name} #${target.card_id} @${target.zone}`; //return;
+    if (attacker.health < 1) throw `dead attacker ${attacker.name} #${attacker.card_id} @${attacker.zone}`; //return;
+    if (attacker.attack < 1) throw `harmless attacker ${attacker.name} #${attacker.card_id}`; //return;
     if (attacker.owner !== game.activePlayer) throw 'wrong turn'; //return; // is there a way to attack on enemy turn ? - UNGORO:WarriorLegendDino(8)
     if (target.owner === attacker.owner) throw 'own unit'; //return; // will fail for Hunter:Misdirection secret, and Ogres
     if (attacker.tags.includes(TAGS.windfury)) {
